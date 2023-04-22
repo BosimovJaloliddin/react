d83a160b96d1ff51edbd6e0196e2b720ffddc685
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-04-22 11:41:33 +0500

    state & setState

--- lessons/src/App.js
+++ lessons/src/App.js
@@ -1,14 +1,45 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      title: "Hellow",
+    };
+  }
   render() {
-    const { data } = this.props;
+    const plus = () => {
+      this.setState({ count: this.state.count + 1 });
+      console.log(this.state.count + 1);
+    };
+    const minus = () => {
+      this.setState({ count: this.state.count - 1 });
+      console.log(this.state.count - 1);
+    };
+    const change = (e) => {
+      this.setState({ title: (this.state.title = e.target.value) });
+      console.log((this.state.title = e.target.value));
+    };
+    const getSelect = (e) => {
+      console.log(e.target.value);
+    };
+    const getCheck = (e) => {
+      console.log(e.target.checked);
+    };
     return (
-      <>
-        <div>
-          {data.id} {data.name}
-        </div>
-      </>
+      <div>
+        <div>{this.state.title}</div>
+        <input onChange={change} type="text" placeholder="name" />
+        <select onChange={getSelect} name="select" id="selsect">
+          <option value="oq">oq</option>
+          <option value="qora">qora</option>
+        </select>
+        <input onChange={getCheck} type="checkbox" />
+        <div>{this.state.count}</div>
+        <button onClick={plus}>+</button>
+        <button onClick={minus}>-</button>
+      </div>
     );
   }
 }
--- lessons/src/index.js
+++ lessons/src/index.js
@@ -4,19 +4,8 @@ import App from "./App";
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
 
-let arr = [
-  { id: 1, name: "Ali1" },
-  { id: 2, name: "Ali2" },
-  { id: 3, name: "Ali3" },
-  { id: 4, name: "Ali4" },
-  { id: 5, name: "Ali5" },
-  { id: 6, name: "Ali6" },
-];
-
 root.render(
   <React.StrictMode>
-    {arr.map((value) => {
-      return <App data={value} />;
-    })}
+    <App />
   </React.StrictMode>
 );
